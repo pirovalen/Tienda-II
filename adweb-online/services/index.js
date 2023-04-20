91d667ec0d076f014eb9b78f0c62adbeb7fd8f76
Sappy <macarenaramdohr@gmail.com>
2023-04-20 12:04:48 -0400

    firebase

---
+++ adweb-online/services/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdzw6q8rAdrFENwTLqFVhhj9_IGYPzgLI",
+  authDomain: "adweb-2f85c.firebaseapp.com",
+  projectId: "adweb-2f85c",
+  storageBucket: "adweb-2f85c.appspot.com",
+  messagingSenderId: "830212848640",
+  appId: "1:830212848640:web:8e35b03eb37e26da692ce5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- adweb-online/src/components/NavBar.vue
+++ adweb-online/src/components/NavBar.vue
@@ -1,46 +1,27 @@
 <template>  
     <div class="container-fluid d-flex align-items-center border">
         <div id="logo">
-            <a v-on:click="Home"><img src="../assets/logo-p.png" alt="" width="250"></a>
+            <a v-on:click="Home"><img src="../assets/logo.png" alt="" width="250"></a>
         </div>
         <div class="d-flex ms-auto me-3">
             <nav>
                 <ul id="list-contenedor" class="d-flex justify-content-around align-items-center">
                     <router-link class="nav-link px-3" to="/HomeRoot">Inicio</router-link>
-                    <router-link class="nav-link px-3" to="/productoPage">Productos</router-link>
-                    <carritoCompra></carritoCompra>
-                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Logout
-                    </button>
-                    <p class="mb-0">{{usuarioConectado}}</p>
+                    <router-link class="nav-link px-3" to="">Cursos</router-link>
+                    <ShowCards></ShowCards>
+                    <router-link class="nav-link px-3" to="">Login</router-link>
+                    <FormLogin></FormLogin>
+                    <!-- <p class="mb-0">{{usuarioConectado}}</p> -->
                 </ul>
             </nav>
         </div>
     </div>
-    <!-- Button trigger modal -->
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-    <div class="modal-content">
-        <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Logout</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-        Esta seguro que desea salir
-        <p>{{usuarioConectado}}</p>
-        </div>
-        <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">No</button>
-        <button type="button" v-on:click="logout" class="btn btn-success" data-bs-dismiss="modal">Si</button>
-        </div>
-    </div>
-    </div>
-    </div>
     </template>
     
     <script>
-    import {mapState} from 'vuex'
+import FormLogin from './FormLogin.vue';
+import ShowCards from './ShowCards.vue';
+
     </script>
     
     
@@ -61,6 +42,6 @@
     
     .list-item{
         cursor: pointer;
-    
     }
+
     </style>
