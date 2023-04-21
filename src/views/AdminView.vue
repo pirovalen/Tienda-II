451e1cdd31e7b7afdfbb7543bfbf27a5108f7d70
Valentriniti <v.rocoarevalo@gmail.com>
2023-04-21 11:06:28 -0400

    agrega adminview y admintable

---
+++ src/views/AdminView.vue
@@ -0,0 +1,15 @@
+<template>
+    <AdminTable />
+      </template>
+      
+      <script>
+      // @ is an alias to /src
+      import AdminTable from '@/components/AdminTable.vue'
+      
+      export default {
+        name: 'HomeView',
+        components: {
+          AdminTable
+        }
+      }
+      </script>
