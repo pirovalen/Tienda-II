9bc3f6e5db57b922c5d62df2bccb9ccca143dd90
Valentina Roco <123381447+Valentriniti@users.noreply.github.com>
2023-04-26 14:03:48 -0400

    Ina2 (#26)
    * agrega carrusel
    
    * arregla login logout navbar
    
    ---------
    
    Co-authored-by: GabiSh19 <123383390+GabiSh19@users.noreply.github.com>

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,11 @@
 <template> 
     <div class="navbar">
         <div class="container-fluid d-flex align-items-center">
-            <div>
+            <div class="me-5">
                 <img src="../assets/logo.png" alt="" width="100">
+            </div>
+            <div>                
+            <p class="mb-0">{{$store.state.usuarioConectado}}</p>
             </div>
             <!-- <div id="logo">
                 <a v-on:click="HomePage"><img src="../assets/logo.png" alt="" width="250"></a>
@@ -13,12 +16,12 @@
                         <router-link class="link-nav px-3" to="/HomeView">Inicio</router-link>
                         <router-link class="link-nav px-3" to="/CoursesView">Cursos</router-link>
                         <router-link class="link-nav px-3" to="/AdminView">Administrador</router-link>
-                        <router-link class="link-nav px-3" to="/">Login</router-link>
-                        <button type="button" class="btn-logout" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <router-link class="link-nav px-3" to="/" v-if="(!$store.state.login)">Login</router-link>
+                        <button type="button" class="btn-logout" data-bs-toggle="modal" data-bs-target="#exampleModal" v-if="($store.state.login)">
                         Logout
                         </button>
                         <!-- <p class="mb-0">{{usuarioConectado}}</p> -->
-                        <p class="mb-0">{{$store.state.usuarioConectado}}</p>
+                    
                     </ul>   
                 </nav>
             </div>
@@ -46,11 +49,16 @@
     </template>
         
     <script>
-    import { auth } from "@/auth/auth.service";
 
+    import { mapState } from 'vuex';
+    import { auth } from "@/auth/auth.service";
+     
+
+   
     export default {
         name: 'NavBar',
-        
+         computed:{
+        ...mapState(['login']),
         methods:{
             async logout() {
                 try {
@@ -64,6 +72,7 @@
             }
         }
     };
+
     </script>
     <style>
     #login{
