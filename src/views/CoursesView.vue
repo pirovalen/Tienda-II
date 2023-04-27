ed99bae6f8e02a29ccc1238453312fbbc5b02fde
GabiSh19 <123383390+GabiSh19@users.noreply.github.com>
2023-04-27 13:53:45 -0400

    agrega estilo a tabla administrador y card (#33)
    * junta archivos
    
    * agrega estilo a tabla administrador y card

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -6,8 +6,8 @@
     <p>Clases online en vivo dictadas por referentes de la industria, enfoque 100% práctico, mentorías personalizadas y acceso a una comunidad de +210.000 estudiantes.</p></div>
     <div class="container mt-5">
         <div class="row justify-content-center py-5" v-if="($store.state.cursos.length) == 0">
-      <div class="col-auto">
-      <fade-loader :loading="loading" :color="color" :size="size" class="mb-5"></fade-loader>
+    <div class="col-auto">
+    <fade-loader :loading="loading" :color="color" :size="size" class="mb-5"></fade-loader>
     </div>
     </div>
         <div class="row" v-else>
@@ -15,14 +15,15 @@
             <div class=" card border-info mb-3 mx-auto" style="width: 18rem;"  >
                 <img :src= "curso.img" class="card-img-top" alt="img">
                 <div class="card-body">
-                    <h5 class="card-title">{{curso.nombre}}</h5>
-                    <p class="card-text">{{curso.descripcion}}</p>
+                    <h5 class="card-title text-center">{{curso.nombre}}</h5>
+                    <p class="card-text text-center">{{curso.descripcion}}</p>
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">Duración: {{curso.duracion  }}</li>
                     <li class="list-group-item">Cupos: {{curso.cupos }}</li>
                     <li class="list-group-item">Precio: $ {{new Intl.NumberFormat('ES', {style: 'currency', currency: 'clp' }).format(curso.precio)}}</li>
                 </ul>
+                <button class=" btnInscribir p-2">Inscribirme</button>
             </div>
         </div>
     </div>
@@ -31,120 +32,110 @@
     
     
     <script>
-    // @ is an alias to /src
     import FadeLoader from 'vue-spinner/src/FadeLoader.vue'
-
-    // import { collection, getDocs } from "firebase/firestore";
-    // import { db } from "@/auth/auth.service";
     import {mapActions, mapState} from 'vuex' 
 
     export default {
-    name: "Home-View",
-    data() {
-        return {
-        color:'#d676ab'
-    }
-    },
+        name: "Home-View",
+        data() {
+            return {
+            color:'#d676ab'
+        }
+        },
 
-    components: {
-         FadeLoader
-      }, 
+        components: {
+            FadeLoader
+        }, 
 
         created(){
             this.getCursos()
         },
+
         methods: {
             ...mapActions(['getCursos'])
-    //   async extraerData() {
+        },
 
-    //     const querySnapshot = await getDocs(collection(db, "adweb-a1409"));
-    //     console.log(querySnapshot)
-    //     querySnapshot.forEach((doc) => {
-    //     console.log(doc.id, " => ", doc.data().nombre);
-    //        this.cursos.push(doc.data());
-    //    });
-       
-    //    console.log(this.cursos);
-    //    console.log(this.cursos.img);
-    //    },
+        computed : {
+            ...mapState(['cursos'])
+        },
 
-    //    mostrarCurso(){
-    //        this.cursos.forEach((element)=>{
+        showAlert(texto1, texto2){
+            Swal.fire({
+                title:texto1,
+                text: texto2,
+                icon: 'success',
+                confirmButtonText: 'Ok'
+            })
+        },
 
-    //            console.log(element.nombre)
-    //        })
-    //    }
-      },
-
-      computed : {
-        ...mapState(['cursos'])
-      },
-
-      showAlert(texto1, texto2){
-        Swal.fire({
-            title:texto1,
-            text: texto2,
-            icon: 'success',
-            confirmButtonText: 'Ok'
-        })
-      },
-
-      async mounted(){
-        this.$store.state.cursos=[]
-        if(this.$store.state.usuarioConectado===''){
-            this.showAlert('No hay usuario conectado', 'Debe loguearse')
-            this.$router.push('/login')
+        async mounted(){
+            this.$store.state.cursos=[]
+            if(this.$store.state.usuarioConectado===''){    
+                this.showAlert('No hay usuario conectado', 'Debe loguearse')
+                this.$router.push('/login')
+            }
         }
-      }
     }
     
     </script>
     
     <style>
-    .titulo{
-    text-align: center;
-    font-family: 'Montserrat', sans-serif;
-    }
-
-    .subtitulo{
-    text-align: center;
-    font-family: 'Montserrat', sans-serif;
-    font-size: 18px;
-    padding-inline: 15rem ;
-    }
-
-    .card{
+        .titulo{
+        text-align: center;
         font-family: 'Montserrat', sans-serif;
-    }
+        }
 
-    .card-img-top{
-    max-width: 8rem;
-    height: 160px;
-    align-self: center;
-    padding-top: 2rem;
-    }
+        .subtitulo{
+        text-align: center;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 18px;
+        padding-inline: 15rem ;
+        }
 
-    .card-body{
-    padding-top: 7rem;
-    }
+        .card{
+            font-family: 'Montserrat', sans-serif;
+        }
 
-    .btn--ver{
-    background-color: #EA4C89;
-    border-radius: 8px;
-    border-style: none;
-    box-sizing: border-box;
-    color: #FFFFFF;
-    cursor: pointer;
-    display: inline-block;
-    font-size: 14px;
-    font-weight: 500;
-    height: 40px;
-    line-height: 20px;
-    margin-top: auto;
-    text-align: center;
-    transition: color 100ms;
-/*     -webkit-user-select: none; */
-    touch-action: manipulation;
-    font-family: 'Montserrat', sans-serif;
-    }
+        .card-img-top{
+        max-width: 8rem;
+        height: 160px;
+        align-self: center;
+        padding-top: 2rem;
+        }
+
+        .card-body{
+        padding-top: 7rem;
+        }
+
+        .btn--ver{
+        background-color: #EA4C89;
+        border-radius: 8px;
+        border-style: none;
+        box-sizing: border-box;
+        color: #FFFFFF;
+        cursor: pointer;
+        display: inline-block;
+        font-size: 14px;
+        font-weight: 500;
+        height: 40px;
+        line-height: 20px;
+        margin-top: auto;
+        text-align: center;
+        transition: color 100ms;
+        touch-action: manipulation;
+        font-family: 'Montserrat', sans-serif;
+        }
+
+        .btnInscribir {
+            background-color: #71c3d7;
+            color: white;
+            border: 1px solid #71c3d7;
+            letter-spacing: 3px;
+        }
+
+        .btnInscribir:hover{
+            background-color: #f082bf;
+        }
+
+
     </style>
